bed78d881a85516b2c2c96435dcf54baed2499e9
Vladimir Lunkin <vova.gold.201427@gmail.com>
2022-03-09 15:40:35 +0300

    https fix and db

--- ProxyServer/server.go
+++ ProxyServer/server.go
@@ -2,19 +2,21 @@ package ProxyServer
 
 import (
 	"bufio"
+	"bytes"
 	"crypto/tls"
 	"errors"
 	"fmt"
 	"github.com/jackc/pgx"
+	"io"
 	"io/ioutil"
 	"log"
 	"math"
 	"math/rand"
-	"net"
 	"net/http"
 	"net/http/httputil"
 	"os/exec"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -25,66 +27,204 @@ type Server struct {
 	DB           *pgx.ConnPool
 }
 
-func (srv *Server) Serve(l net.Listener) error {
-	for {
-		rwc, err := l.Accept()
-		if err != nil {
-			return err
-		}
-
-		conn := srv.newConn(rwc)
-		go conn.serve()
-	}
-}
-
 func (srv *Server) ListenAndServe() error {
-	addr := srv.Addr
-	if addr == "" {
-		addr = ":http"
+	server := http.Server{
+		Addr: srv.Addr,
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			srv.proxy(w, r)
+		}),
 	}
-	ln, err := net.Listen("tcp", addr)
-	if err != nil {
-		return err
-	}
-	return srv.Serve(ln)
+
+	return server.ListenAndServe()
 }
 
-func (srv *Server) saveToDB(req *http.Request, resp *http.Response) error {
-	var reqId int32
-	reqHeaders := headersToString(req.Header)
-	reqBody, err := ioutil.ReadAll(req.Body)
+func (srv *Server) proxy(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodConnect {
+		srv.proxyHTTPS(w, r)
+	} else {
+		srv.proxyHTTP(w, r)
+	}
+}
+
+func (srv *Server) proxyHTTP(w http.ResponseWriter, r *http.Request) {
+	r.Header.Del("Proxy-Connection")
+
+	reqId, err := srv.saveRequest(r)
+	if err != nil {
+		log.Printf("fail save to db: %v", err)
+	}
+
+	resp, err := http.DefaultTransport.RoundTrip(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+	defer resp.Body.Close()
+
+	for key, values := range resp.Header {
+		for _, value := range values {
+			w.Header().Add(key, value)
+		}
+	}
+
+	err = srv.saveResponse(reqId, resp)
+	if err != nil {
+		log.Printf("fail save to db: %v", err)
+	}
+
+	w.WriteHeader(resp.StatusCode)
+	_, err = io.Copy(w, resp.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (srv *Server) proxyHTTPS(w http.ResponseWriter, r *http.Request) {
+	hijacker, ok := w.(http.Hijacker)
+	if !ok {
+		log.Println("Hijacking not supported")
+		http.Error(w, "Hijacking not supported", http.StatusInternalServerError)
+		return
+	}
+
+	localConn, _, err := hijacker.Hijack()
+	if err != nil {
+		log.Printf("hijacking error: %v", err)
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+	}
+
+	_, err = localConn.Write([]byte("HTTP/1.1 200 Connection established\r\n\r\n"))
+	if err != nil {
+		log.Printf("handshaking failed: %v", err)
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		localConn.Close()
+		return
+	}
+	defer localConn.Close()
+
+	host := strings.Split(r.Host, ":")[0]
+
+	tlsConfig, err := generateTLSConfig(host, r.URL.Scheme)
+	if err != nil {
+		log.Printf("error getting cert: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	tlsLocalConn := tls.Server(localConn, &tlsConfig)
+	err = tlsLocalConn.Handshake()
+	if err != nil {
+		tlsLocalConn.Close()
+		log.Printf("handshaking failed: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer tlsLocalConn.Close()
+
+	remoteConn, err := tls.Dial("tcp", r.URL.Host, &tlsConfig)
 	if err != nil {
 		log.Println(err)
-		return err
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
 	}
+	defer remoteConn.Close()
+
+	reader := bufio.NewReader(tlsLocalConn)
+	request, err := http.ReadRequest(reader)
+	if err != nil {
+		log.Printf("error getting request: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	requestByte, err := httputil.DumpRequest(request, true)
+	if err != nil {
+		log.Printf("failed to dump request: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	_, err = remoteConn.Write(requestByte)
+	if err != nil {
+		log.Printf("failed to write request: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	serverReader := bufio.NewReader(remoteConn)
+	response, err := http.ReadResponse(serverReader, request)
+	if err != nil {
+		log.Printf("failed to read response: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	rawResponse, err := httputil.DumpResponse(response, true)
+	if err != nil {
+		log.Printf("failed to dump response: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	_, err = tlsLocalConn.Write(rawResponse)
+	if err != nil {
+		log.Printf("fail to write response: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	request.URL.Scheme = "https"
+	hostAndPort := strings.Split(r.URL.Host, ":")
+	request.URL.Host = hostAndPort[0]
+
+	reqId, err := srv.saveRequest(request)
+	if err != nil {
+		log.Printf("fail save to db: %v", err)
+	}
+
+	err = srv.saveResponse(reqId, response)
+	if err != nil {
+		log.Printf("fail save to db: %v", err)
+	}
+}
+
+func (srv *Server) saveRequest(r *http.Request) (int32, error) {
+	var reqId int32
+	reqHeaders := headersToString(r.Header)
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return 0, err
+	}
+	r.Body = ioutil.NopCloser(bytes.NewBuffer(reqBody))
 
 	err = srv.DB.QueryRow(`INSERT INTO request (method, scheme, host, path, header, body)
 			values ($1, $2, $3, $4, $5, $6) RETURNING id`,
-		req.Method,
-		req.URL.Scheme,
-		req.URL.Host,
-		req.URL.Path,
+		r.Method,
+		r.URL.Scheme,
+		r.URL.Host,
+		r.URL.Path,
 		reqHeaders,
 		string(reqBody)).Scan(&reqId)
+
+	return reqId, err
+}
+
+func (srv *Server) saveResponse(reqId int32, resp *http.Response) error {
+	var respId int32
+	respHeaders := headersToString(resp.Header)
+	respBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Println(err)
 		return err
 	}
+	resp.Body = ioutil.NopCloser(bytes.NewBuffer(respBody))
 
-	//insertRespQuery := `INSERT INTO response (req_id, code, resp_message, header, body)
-	//values ($1, $2, $3, $4, $5) RETURNING id`
-	//var respId int32
-	//respHeaders := headersToString(resp.Header)
-	//respBody, err := ioutil.ReadAll(resp.Body)
-	//if err != nil {
-	//	return err
-	//}
-	//err = srv.DB.QueryRow(insertRespQuery, reqId, resp.StatusCode, resp.Status[4:], respHeaders, respBody).Scan(&respId)
-	//if err != nil {
-	//	return err
-	//}
-
-	return nil
+	return srv.DB.QueryRow(`INSERT INTO response (req_id, code, resp_message, header, body)
+	values ($1, $2, $3, $4, $5) RETURNING id`,
+		reqId,
+		resp.StatusCode,
+		resp.Status[4:],
+		respHeaders,
+		respBody).Scan(&respId)
 }
 
 func headersToString(headers http.Header) string {
@@ -95,103 +235,6 @@ func headersToString(headers http.Header) string {
 		}
 	}
 	return stringHeaders
-}
-
-type conn struct {
-	server *Server
-	rwc    net.Conn
-}
-
-func (srv *Server) newConn(rwc net.Conn) *conn {
-	c := &conn{
-		server: srv,
-		rwc:    rwc,
-	}
-	return c
-}
-
-func (c *conn) serve() {
-	defer c.rwc.Close()
-
-	if d := c.server.ReadTimeout; d > 0 {
-		c.rwc.SetReadDeadline(time.Now().Add(d))
-	}
-	if d := c.server.WriteTimeout; d > 0 {
-		c.rwc.SetWriteDeadline(time.Now().Add(d))
-	}
-
-	reader := bufio.NewReader(c.rwc)
-	req, err := http.ReadRequest(reader)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	proxyConn, err := Dial(c.rwc, req)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer proxyConn.Close()
-
-	resp, err := HandleProxy(proxyConn, req)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer resp.Body.Close()
-
-	respByte, err := httputil.DumpResponse(resp, true)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	_, err = c.rwc.Write(respByte)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	log.Println("Request:", req.Method, req.URL, "Response:", resp.Status)
-
-	err = c.server.saveToDB(req, resp)
-	if err != nil {
-		return
-	}
-}
-
-func Dial(clientConn net.Conn, r *http.Request) (net.Conn, error) {
-	if r.Method == http.MethodConnect {
-		_, err := clientConn.Write([]byte("HTTP/1.1 200 Connection established\r\n\r\n"))
-		if err != nil {
-			return nil, err
-		}
-
-		tlsConfig, err := generateTLSConfig(r.Host, r.URL.Scheme)
-		if err != nil {
-			return nil, err
-		}
-
-		tlsLocalConn := tls.Server(clientConn, &tlsConfig)
-		err = tlsLocalConn.Handshake()
-		if err != nil {
-			return nil, err
-		}
-
-		remoteConn, err := tls.Dial("tcp", r.URL.Host, &tlsConfig)
-		if err != nil {
-			return nil, err
-		}
-
-		return remoteConn, nil
-	}
-
-	remoteConn, err := net.Dial("tcp", r.URL.Host+":80")
-	if err != nil {
-		return nil, err
-	}
-	return remoteConn, nil
 }
 
 func generateTLSConfig(host, URL string) (tls.Config, error) {
@@ -217,19 +260,4 @@ func generateTLSConfig(host, URL string) (tls.Config, error) {
 		Certificates: []tls.Certificate{tlsCert},
 		ServerName:   URL,
 	}, nil
-}
-
-func HandleProxy(c net.Conn, req *http.Request) (*http.Response, error) {
-	err := req.WriteProxy(c)
-	if err != nil {
-		return nil, err
-	}
-
-	reader := bufio.NewReader(c)
-	resp, err := http.ReadResponse(reader, req)
-	if err != nil {
-		return nil, err
-	}
-
-	return resp, nil
 }
